b7dc48c8c12d53bd71f22c12fc346c65b70a846a
Alexander Schmitz <arschmitz@gmail.com>
2016-12-08 15:42:14 -0500

    Support ember fastboot

--- index.js
+++ index.js
@@ -14,28 +14,30 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    var options = app.options['ember-cli-jquery-ui'] || {};
-    var theme = options.theme || "base";
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      var options = app.options['ember-cli-jquery-ui'] || {};
+      var theme = options.theme || "base";
 
-    app.import(path.join(app.bowerDirectory, 'jquery-ui', 'jquery-ui.js'));
+      app.import(path.join(app.bowerDirectory, 'jquery-ui', 'jquery-ui.js'));
 
-    if (theme != 'none') {
-      var cssFileDir = path.join(app.bowerDirectory, 'jquery-ui', 'themes', theme);
-      var cssFiles = fs.readdirSync(cssFileDir);
+      if (theme != 'none') {
+        var cssFileDir = path.join(app.bowerDirectory, 'jquery-ui', 'themes', theme);
+        var cssFiles = fs.readdirSync(cssFileDir);
 
-      cssFiles.forEach(function(file) {
-        if (/^.*\.css$/.test(file) && !/^.*\.min\.css$/.test(file))
-          app.import(path.join(cssFileDir, file));
-      });
-
-      var imgFileDir = path.join(app.bowerDirectory, 'jquery-ui', 'themes', theme, 'images');
-      var imgFiles = fs.readdirSync(imgFileDir);
-
-      imgFiles.forEach(function(file) {
-        app.import(path.join(imgFileDir, file), {
-          destDir: "/assets/images"
+        cssFiles.forEach(function(file) {
+          if (/^.*\.css$/.test(file) && !/^.*\.min\.css$/.test(file))
+            app.import(path.join(cssFileDir, file));
         });
-      });
+
+        var imgFileDir = path.join(app.bowerDirectory, 'jquery-ui', 'themes', theme, 'images');
+        var imgFiles = fs.readdirSync(imgFileDir);
+
+        imgFiles.forEach(function(file) {
+          app.import(path.join(imgFileDir, file), {
+            destDir: "/assets/images"
+          });
+        });
+      }
     }
   }
 };
