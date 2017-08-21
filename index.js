9f6091ce2977674a667b0aff1be7b2dbd9ea349c
Alexander Schmitz <arschmitz@gmail.com>
2017-08-21 11:10:01 -0400

    Update for fastboot 1.0

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 /* jshint node: true */
 'use strict';
-
-var fs = require('fs');
 var path = require('path');
 
 module.exports = {
   name: 'ember-cli-jquery-ui',
+
+  options: {
+    nodeAssets: {
+      'jquery-ui': {
+        vendor: ['jquery-ui.js'],
+        processTree(input) {
+          return fastbootTransform(input);
+        }
+      }
+    }
+  },
 
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
@@ -14,30 +23,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      var options = app.options['ember-cli-jquery-ui'] || {};
-      var theme = options.theme || "base";
-
-      app.import(path.join(app.bowerDirectory, 'jquery-ui', 'jquery-ui.js'));
-
-      if (theme != 'none') {
-        var cssFileDir = path.join(app.bowerDirectory, 'jquery-ui', 'themes', theme);
-        var cssFiles = fs.readdirSync(cssFileDir);
-
-        cssFiles.forEach(function(file) {
-          if (/^.*\.css$/.test(file) && !/^.*\.min\.css$/.test(file))
-            app.import(path.join(cssFileDir, file));
-        });
-
-        var imgFileDir = path.join(app.bowerDirectory, 'jquery-ui', 'themes', theme, 'images');
-        var imgFiles = fs.readdirSync(imgFileDir);
-
-        imgFiles.forEach(function(file) {
-          app.import(path.join(imgFileDir, file), {
-            destDir: "/assets/images"
-          });
-        });
-      }
-    }
+    app.import('vendor/jquery-ui/jquery-ui.js');
   }
 };
